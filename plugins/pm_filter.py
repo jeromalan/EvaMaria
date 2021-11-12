a0c61ab15e1e3a361c5a6ba52a846497e64ff851
jeromalan <73242169+jeromalan@users.noreply.github.com>
2021-11-13 00:20:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -394,7 +394,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     file_id=file_id,
                     caption=f_caption
                     )
-                await query.answer('Check PM, I have sent files in pm',show_alert = True)
+                await query.answer('Files ഞാൻ PM ൽ അയച്ചിട്ടുണ്ട് ഒന്നു പോയി നോക്കിക്കേ 😁\n\nCheck PM, I have sent files in pm',show_alert = True)
         except UserIsBlocked:
             await query.answer('Unblock the bot mahn !',show_alert = True)
         except PeerIdInvalid:
